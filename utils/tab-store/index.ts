9fe3de72bda4a559e072e8d7761648356ba1baec
Tony Hu <tonyhu@xmind.org>
2025-07-10 16:06:45 +0800

    refactor(firefox): tab storage handling for Firefox compatibility

--- utils/tab-store/index.ts
+++ utils/tab-store/index.ts
@@ -1,6 +1,7 @@
 import { customRef, ref, toRaw, watch } from 'vue'
 import { storage } from 'wxt/utils/storage'
 
+import { TAB_STORE_STORAGE_KEY_PREFIX } from '../constants'
 import { debounce } from '../debounce'
 import { Base64ImageData } from '../image'
 import { lazyInitialize } from '../memo'
@@ -10,12 +11,15 @@ import type { HistoryItemV1 } from './history'
 
 export { HistoryItemV1 }
 
-async function defineTabValue<T>(key: string, defaultValue: T) {
-  let sessionKey = `session:tab-store-${key}` as `${'local' | 'session'}:tab-store-${string}`
-  if (import.meta.env.FIREFOX) {
-    // Firefox does not support session storage in content scripts
-    sessionKey = `local:tab-store-${key}` as `local:tab-store-${string}`
-  }
+function constructStorageKey(tabId: number, key: string) {
+  const scope = import.meta.env.FIREFOX
+    ? 'local'
+    : 'session'
+  return `${scope}:${TAB_STORE_STORAGE_KEY_PREFIX}${tabId}-${key}` as `${'local' | 'session'}:${string}`
+}
+
+async function defineTabValue<T>(tabId: number, key: string, defaultValue: T) {
+  const sessionKey = constructStorageKey(tabId, key)
   const valueInStorageStr = await storage.getItem<string>(sessionKey)
   const valueInStorage = valueInStorageStr ? JSON.parse(valueInStorageStr) : defaultValue
   const v = ref(valueInStorage)
@@ -59,14 +63,14 @@ async function _getTabStore() {
   const { tabId, faviconUrl, url, title } = await c2bRpc.getTabInfo()
   if (!tabId) throw new Error('no tab id')
   return {
-    currentTabId: await defineTabValue('currentTabId', tabId),
-    showContainer: await defineTabValue(`showContainer-${tabId}`, false),
-    showSetting: await defineTabValue<ShowSettingsParams>(`showSetting-${tabId}`, { show: false }),
-    chatHistory: await defineTabValue(`chatHistory-${tabId}`, [] as ChatHistory),
-    pageSummary: await defineTabValue(`summary-${tabId}`, { content: '', summary: '' } as PageSummary),
-    contextTabIds: await defineTabValue(`contextTabs-${tabId}`, [tabId] as number[]),
-    contextImages: await defineTabValue(`contextImages-${tabId}`, [] as Base64ImageData[]),
-    tabInfo: await defineTabValue(`tabInfo-${tabId}`, {
+    currentTabId: await defineTabValue(tabId, 'currentTabId', tabId),
+    showContainer: await defineTabValue(tabId, `showContainer`, false),
+    showSetting: await defineTabValue<ShowSettingsParams>(tabId, `showSetting`, { show: false }),
+    chatHistory: await defineTabValue(tabId, `chatHistory`, [] as ChatHistory),
+    pageSummary: await defineTabValue(tabId, `summary`, { content: '', summary: '' } as PageSummary),
+    contextTabIds: await defineTabValue(tabId, `contextTabs`, [tabId] as number[]),
+    contextImages: await defineTabValue(tabId, `contextImages`, [] as Base64ImageData[]),
+    tabInfo: await defineTabValue(tabId, `tabInfo`, {
       url,
       title,
       faviconUrl,
@@ -75,3 +79,18 @@ async function _getTabStore() {
 }
 
 export const getTabStore = lazyInitialize(_getTabStore)
+
+// this is a workaround for firefox to clear the tab store when the tab is closed
+export const getTabKeys = (tabId: number) => {
+  const keys = [
+    'currentTabId',
+    'showContainer',
+    'showSetting',
+    'chatHistory',
+    'summary',
+    'contextTabs',
+    'contextImages',
+    'tabInfo',
+  ]
+  return keys.map((key) => constructStorageKey(tabId, key))
+}
